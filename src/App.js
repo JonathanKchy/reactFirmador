1a774b70d763727977f8a5eb966f9d84cc9d7db8
Andres <andres.jaag@hotmail.es>
2022-09-26 11:57:55 -0500

    see pdf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+
 import freeCodeCampLogo from './imagenes/sodig-sin-fondo.png'
 import Boton from './componentes/Boton';
 import Contador from './componentes/Contador';
 import { useState } from 'react';
 
+//import { Viewer } from '@react-pdf-viewer/core';
+import { Worker } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import pdfFile from './pdf/pki.pdf';
+
+import { Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core';
+
 function App() {
 
+  const [defaultPdfFile]=useState(pdfFile);
+
+  const defaultLayoutPluginInstance=defaultLayoutPlugin();
   const [numClics,setNumClics]= useState(0);
 
   const manejarClic = () =>{
@@ -23,6 +36,15 @@ function App() {
         className='freecodecamp-logo'
         src={freeCodeCampLogo}
         alt='Logo de freeCodeCamp' />
+      </div>
+      
+      <div className='pdf-container'>
+        {defaultPdfFile&&<><Worker workerUrl="https://unpkg.com/pdfjs-dist@2.15.349/build/pdf.worker.min.js">
+            <Viewer fileUrl={defaultPdfFile}
+              //plugins={[defaultLayoutPluginInstance]}
+              //defaultScale={SpecialZoomLevel.PageFit}
+              />
+        </Worker></>}
       </div>
       <div className='contenedor-principal'>
         <Contador numClics={numClics} />
