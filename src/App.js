da28df0fe3c6d05f9017f2b7b5ec4d8807a98d37
Andres <andres.jaag@hotmail.es>
2022-09-26 12:09:46 -0500

    see pdf.3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 
 import { Viewer } from '@react-pdf-viewer/core';
 import { Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+//import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import pdfFile from './pdf/pki.pdf';
